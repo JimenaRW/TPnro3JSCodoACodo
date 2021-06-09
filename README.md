551b6970226a0fb20fe423413a7c575cae050388
Jimena <jimena.witen@gmail.com>
2021-06-09 19:14:53 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Trabajo Práctico nro 3 de JavaScript - Codo a Codo
---
+++ js/app.js
@@ -0,0 +1,10 @@
+let alumnos=["luis","navas"];
+
+for(let a=0; a >alumnos; a++){
+    console.log(alumnos[a]);
+};
+
+let datosLuis=["luis","navas",49,true];
+datosLuis.forEach(dato => {
+    console.log(dato);
+});
