6150b4736b8f080e3fb5a7cec29e0d4bf001d3be
Jimena <jimena.witen@gmail.com>
2021-07-04 22:49:58 -0300

    terminado

--- js/app.js
+++ js/app.js
@@ -1,10 +1,52 @@
-let alumnos=["luis","navas"];
+let usuarios = [];
+let nombre = [];
+let edad = [];
+let contador;
+let control = false;
+let mayor;
 
-for(let a=0; a >alumnos; a++){
-    console.log(alumnos[a]);
-};
+while(control == false) {
 
-let datosLuis=["luis","navas",49,true];
-datosLuis.forEach(dato => {
-    console.log(dato);
-});
+    contador = parseInt(prompt("Buenas! Diga la cantidad de datos que va a utilizar: "));
+    if (!isNaN(contador)) {
+        control = true;
+    }
+}
+
+control = false;
+
+for (let i = 0; i < contador; i++) {
+
+    let itemNombre = prompt("Ingrese su nombre para visualizar la página: ");
+
+    nombre.push(itemNombre)
+
+    while(control == false) {
+        let itemEdad = parseFloat(prompt(`Verificaremos su edad ${nombre[i]}, por favor ingresela a continuación: `));
+
+        if (!isNaN(itemEdad)) {
+            edad.push(itemEdad)
+            control = true;
+        }
+        else {
+            console.log(`Hubo un error en el numero que ingreso, vuelta a intentarlo ${nombre[i]}`);
+        }
+    }
+
+    control = false;
+    
+}
+
+usuarios.push({
+    nombres: [...nombre],
+    edades: [...edad]
+})
+
+for (let i = 0; i < contador; i++) {
+    if(usuarios[0].edades[i] > 0 && usuarios[0].edades[i] < 18) {
+        alert(`Eres menor de edad aún, ${usuarios[0].nombres[i]}!`);
+    } else if (usuarios[0].edades[i] >= 18) {
+        alert(`Eres mayor de edad, ${usuarios[0].nombres[i]}!`);
+        mayor = usuarios[0].nombres[i]
+    }  
+}
